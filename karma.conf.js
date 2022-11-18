d4c55efb76d44a87a2355a3e990d50aabc1cc72a
Jhonatan(Principal) <jhonatanzapatarol@gmail.com>
2022-11-18 16:29:02 -0500

    commit

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/mapas-app'),
+      dir: require('path').join(__dirname, './coverage/mapasapp'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'mapas-app'`, () => {
+  it(`should have as title 'mapasapp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('mapas-app');
+    expect(app.title).toEqual('mapasapp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('mapas-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('mapasapp app is running!');
   });
 });
